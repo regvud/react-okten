a6c789222ca7fbd0c9ae84f944d45a74d2548678
regvud <silveron.regvud@gmail.com>
2023-07-06 17:33:27 +0300

    Posts added

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,9 @@
 //     з jsonplaceholder отримати всі пости в компоненту Posts.js
+const Posts = (post) => {
 
+}
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts =>)
 //     відобразити кожного інформацію (id,title) з кожного поста (компонента Post)
 //     Зробити кнопку вибора поста, при натисканні на яку в Posts.js ви покажете детальну інфомацію про пост(всю інфу)
