32a35beef45267579d8a0b2315cae28ab9dac7ae
regvud <silveron.regvud@gmail.com>
2023-07-11 16:14:39 +0300

    lesson3 commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
-import React, {createContext, useEffect, useState} from "react";
+import React, { useEffect, useState} from "react";
 import Users from "./components/Users/Users";
-import UserPosts from "./components/UserPosts/UserPosts";
-import Child1 from "./components/Child1/Child1";
-import Child2 from "./components/Child2/Child2";
 
 // property drill
 
@@ -14,47 +11,19 @@ import Child2 from "./components/Child2/Child2";
 // UserDetails - яка відображає інформацію id,name,username,email,phone,website,
 // UserAddress - яка відображає інформацію street suite city,
 // Company - яка відображає name,catchPhrase,bs
-//
-// state lifting
-// Є компонента Арр. В середині неї є UsersComponent, який отримує наданні з https://jsonplaceholder.typicode.com/users
-//     Кожен юзер - окрема компонента UserComponent,в якій є кнопка show posts, при натисканні на яку робиться state lifting, а саме - в компоненті App відображаються пости того юзера, на кнопку якого клікнули.
-//
-//     context
-// Зробити структуру компонентів -
-// App
-// Child1                              Child2
-// SubChild1_1    SubChild1_2          SubChild2_1    SubChild2_2
-//
-// Тепер завдання:
-//     З SubChild2_2 передати данні (будь-які) в SubChild1_1
-//
-//
-//
-// ***
-// Я б для себе рекомендував зробити наступну штуку з завданням з subchild:
-//     Зробити його спочатку без використання контексту, а потім переробити з ним. Так краще дійде суть
 
-const ChildContext = createContext(null)
 
 function App() {
     const [users, setUsers] = useState([])
-    const [userPosts, setUserPosts] = useState([])
-    const [userId, setUserId] = useState(null)
-    const [subChild, setSubChild] = useState(null)
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(users => setUsers(users))
     }, [])
 
-    console.log(ChildContext)
     return (
         <>
-            <Users users={users} setUserId={setUserId}/>
+            <Users users={users}/>
             {/*<UserPosts userId={userId} userPosts={userPosts} setUserPosts={setUserPosts}/>*/}
-            {/*<ChildContext.Provider value={{subChild, setSubChild}}>*/}
-            {/*    <Child1/>*/}
-            {/*    <Child2/>*/}
-            {/*</ChildContext.Provider>*/}
         </>
     );
 }
--- src/components/Users/User/User.js
+++ src/components/Users/User/User.js
@@ -4,14 +4,13 @@ import UserAddress from "./UserComponents/UserAddress";
 import Company from "./UserComponents/Company";
 import styles from './UserComponents/Users.module.css'
 
-const User = ({user, setUserId}) => {
+const User = ({user}) => {
     const {id, email, name, username, address, company} = user
     return (
         <div className={styles.block}>
             <UserDetails id={id} email={email} name={name} username={username}/>
             <UserAddress address={address}/>
             <Company company={company}/>
-            <button onClick={() => setUserId(id)}>Show Posts</button>
         </div>
     );
 };
