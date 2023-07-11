6ef2079ede43bb914d5a406e3c63c28f429fc221
regvud <silveron.regvud@gmail.com>
2023-07-11 17:24:04 +0300

    lesson3 context commit

--- src/components/Users/User/User.js
+++ src/components/Users/User/User.js
@@ -16,18 +16,3 @@ const User = ({user}) => {
 };
 
 export default User;
-
-// import React from 'react';
-// import UserDetails from "./UserDetails";
-// import UserAddress from "./UserAddress";
-//
-// const User = ({user}) => {
-//     return (
-//         <div>
-//             <UserDetails id={user.id} email={user.email} name={user.name} username={user.username}/>
-//             <UserAddress address={user.address}/>
-//         </div>
-//     );
-// };
-//
-// export default User;
