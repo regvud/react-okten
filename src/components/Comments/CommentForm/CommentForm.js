410cd1a7a63acc5af3302100270bf050787d2e0a
regvud <silveron.regvud@gmail.com>
2023-07-17 19:21:12 +0300

    lesson4_comments edit

--- src/components/Comments/CommentForm/CommentForm.js
+++ src/components/Comments/CommentForm/CommentForm.js
@@ -1,14 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import {useForm} from "react-hook-form";
 
-const CommentForm = ({handleSubmit}) => {
-    const {register, reset} = useForm();
+const CommentForm = () => {
+    const {register, reset, handleSubmit} = useForm();
 
 
+    // <h4>{postId}</h4>
+    // <h4>{id}</h4>
+    // <h4>{name}</h4>
+    // <h4>{email}</h4>
+    // <h4>{body}</h4>
+
+    const save = (user) => {
+        console.log(user)
+    }
 
     return (
-        <form onSubmit={handleSubmit}>
-
+        <form onSubmit={handleSubmit(save)}>
+            <label>postId <input type="text" placeholder={'postId'} {...register('postId')}/></label>
+            <label>id <input type="text" placeholder={'id'} {...register('id')}/></label>
+            <label>name <input type="text" placeholder={'name'} {...register('name')}/></label>
+            <label>email <input type="text" placeholder={'email'} {...register('email')}/></label>
+            <label>body <input type="text" placeholder={'body'} {...register('body')}/></label>
+            <button>save</button>
         </form>
     );
 };
--- src/components/Comments/CommentsContainer/CommentsContainer.js
+++ src/components/Comments/CommentsContainer/CommentsContainer.js
@@ -5,22 +5,20 @@ import CommentForm from "../CommentForm/CommentForm";
 const CommentsContainer = () => {
     const [comments, setComments] = useState([])
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        useEffect(() => {
-            fetch('https://jsonplaceholder.typicode.com/comments', {
-                method: 'POST',
-                headers: {
-                    'content-type': 'Application/json'
-                },
-                body: JSON.stringify(comments)
-            })
-                .then(value => value.json())
-                .then(comments => console.log(comments))
-        }, [])
-
-    }
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault()
+    //
+    //     await fetch('https://jsonplaceholder.typicode.com/comments', {
+    //         method: 'POST',
+    //         headers: {
+    //             'content-type': 'Application/json'
+    //         },
+    //         body: JSON.stringify(comments)
+    //     })
+    //         .then(value => value.json())
+    //         .then(comments => console.log(comments))
+    //
+    // }
     return (
         <div>
             <CommentForm handleSubmit={handleSubmit}/>
