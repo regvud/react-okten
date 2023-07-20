dc199391ae51969daeb1ed84979d614b0cb53c23
regvud <silveron.regvud@gmail.com>
2023-07-20 12:26:04 +0300

    lesson5_cars

--- src/components/Cars/CarForm/CarForm.js
+++ src/components/Cars/CarForm/CarForm.js
@@ -3,12 +3,22 @@ import {useForm} from "react-hook-form";
 
 const CarForm = () => {
     const {register, reset, handleSubmit} = useForm();
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const save = (car) => {
+        console.log(car)
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label><input type="text" {...register({})}/></label>
-            <label><input type="text"/></label>
-            <label><input type="text"/></label>
-            <label><input type="text"/></label>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>id<input type="text" {...register('id')}/></label>
+            <label>brand<input type="text" {...register('brand')}/></label>
+            <label>price<input type="text"{...register('price')}/></label>
+            <label>year<input type="text"{...register('year')}/></label>
+            <button onClick={save}>Save</button>
         </form>
     );
 };
--- src/components/Cars/CarsContainer/CarsContainer.js
+++ src/components/Cars/CarsContainer/CarsContainer.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import Cars from "../Cars";
+import CarForm from "../CarForm/CarForm";
 
 const CarsContainer = () => {
 
     return (
         <div>
-
+            <CarForm/>
             <Cars/>
         </div>
     );
