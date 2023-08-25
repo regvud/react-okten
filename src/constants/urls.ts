10089bdccb044f2a2f9403af37c899e71f9e1c89
regvud <silveron.regvud@gmail.com>
2023-08-25 11:59:34 +0300

    add pagination

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,9 +1,11 @@
 const baseURL = process.env.REACT_APP_API
 
+
+const cars = '/cars'
 const urls = {
     cars: {
-        base: '/cars',
-        byID: (id: number): string => `${urls.cars.base}/${id}`
+        base: cars,
+        byID: (id: number): string => `${cars}/${id}`
     }
 }
 
