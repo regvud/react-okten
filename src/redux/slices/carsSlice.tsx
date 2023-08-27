56d93195e21db9aafe2f9460f547ddb7dbbeb1b6
regvud <silveron.regvud@gmail.com>
2023-08-27 12:58:37 +0300

    add pagination

--- src/redux/slices/carsSlice.tsx
+++ src/redux/slices/carsSlice.tsx
@@ -1,4 +1,101 @@
-// import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {ICar} from "../../interfaces/carInterface";
+import {carService} from "../../services/carService";
+import {AxiosError} from "axios";
 
-// const carsSlice = createSlice({
+interface IState {
+    cars: ICar[],
+    carForUpdate: ICar
+}
+
+const initialState: IState = {
+    cars: [],
+    carForUpdate: null
+}
+
+const getAll = createAsyncThunk<ICar[], void>(
+    'carSlice/getAll',
+    async (_, {rejectWithValue}) => {
+        try {
+            const {data} = await carService.getAll()
+            return data
+        } catch (e) {
+            const err = e as AxiosError
+            rejectWithValue(err.response.data)
+        }
+    }
+)
+
+const create = createAsyncThunk<void, ICar>(
+    'carSlice/create',
+    async (car, {rejectWithValue, dispatch}) => {
+        try {
+            await carService.create(car)
+            await dispatch(getAll())
+        } catch (e) {
+            const err = e as AxiosError
+            rejectWithValue(err.response.data)
+        }
+    }
+)
+
+const update = createAsyncThunk<void, { car: ICar, id: number }>(
+    'carSlice/update',
+    async ({car, id}, {rejectWithValue, dispatch}) => {
+        try {
+            await carService.updateByID(car, id)
+            await dispatch(getAll())
+        } catch (e) {
+            const err = e as AxiosError
+            rejectWithValue(err.response.data)
+        }
+    }
+)
+
+const remove = createAsyncThunk<void, number>(
+    'carSlice/remove',
+    async (id, {rejectWithValue, dispatch}) => {
+        try {
+            await carService.deleteByID(id)
+            await dispatch(getAll())
+        } catch (e) {
+            const err = e as AxiosError
+            rejectWithValue(err.response.data)
+        }
+    }
+)
+
+
+const carSlice = createSlice({
+    name: 'carSlice',
+    initialState,
+    reducers: {
+        setCarForUpdate: (state, action: PayloadAction<{ car: ICar }>) => {
+            state.carForUpdate = action.payload.car
+        }
+    },
+    extraReducers: builder =>
+        builder
+            .addCase(getAll.fulfilled, (state, action) => {
+                state.cars = action.payload
+            })
+            .addCase(update.fulfilled, (state) => {
+                state.carForUpdate = null
+            })
+
 })
+
+const {reducer: carReducer, actions} = carSlice;
+
+const carActions = {
+    ...actions,
+    getAll,
+    create,
+    update,
+    remove
+}
+
+export {
+    carReducer,
+    carActions
+}
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,8 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {carReducer} from "./slices/carsSlice";
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        cars: carReducer
+    }
 })
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
 
 export {
     store
